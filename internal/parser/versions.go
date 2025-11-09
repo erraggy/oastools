3893d6b61e29aab25764b5182b8669c7a57d41cf
Robbie Coleman <robbie@robnrob.com>
2025-11-08 20:50:20 -0800

    adds doc comments to new versions.go

--- internal/parser/versions.go
+++ internal/parser/versions.go
@@ -1,18 +1,30 @@
 package parser
 
+// OASVersion represents each canonical version of the OpenAPI Specification that may be found at:
+// https://github.com/OAI/OpenAPI-Specification/releases
 type OASVersion int
 
 const (
 	unknown OASVersion = iota
+	// OASVersion20 OpenAPI Specification Version 2.0 (Swagger)
 	OASVersion20
+	// OASVersion300 OpenAPI Specification Version 3.0.0
 	OASVersion300
+	// OASVersion301  OpenAPI Specification Version 3.0.1
 	OASVersion301
+	// OASVersion302  OpenAPI Specification Version 3.0.2
 	OASVersion302
+	// OASVersion303  OpenAPI Specification Version 3.0.3
 	OASVersion303
+	// OASVersion304  OpenAPI Specification Version 3.0.4
 	OASVersion304
+	// OASVersion310  OpenAPI Specification Version 3.1.0
 	OASVersion310
+	// OASVersion311  OpenAPI Specification Version 3.1.1
 	OASVersion311
+	// OASVersion312  OpenAPI Specification Version 3.1.2
 	OASVersion312
+	// OASVersion320  OpenAPI Specification Version 3.2.0
 	OASVersion320
 )
 
@@ -46,11 +58,13 @@ func (v OASVersion) String() string {
 	return "unknown"
 }
 
+// IsValid returns true is this is a valid version
 func (v OASVersion) IsValid() bool {
 	_, ok := versionToString[v]
 	return ok
 }
 
+// ParseVersion will attempt to parse the string s into an OASVersion, and returns false if not valid
 func ParseVersion(s string) (OASVersion, bool) {
 	v, ok := stringToVersion[s]
 	return v, ok
